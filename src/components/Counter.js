81b01abccb84ee50d5e211f0dd38cde9a55275d1
bonveeee <bonochieng@gmail.com>
2022-07-16 16:43:16 +0300

    Done.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,10 +4,21 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    console.log(count);
+    // => 0
+  
+    setCount((currentCount) => currentCount + 1);
+    // equivalent to setCount(0 + 1)
+  
+    console.log(count);
+    // => 0
+  
+    
   }
-
+  
   return <button onClick={increment}>I have been clicked {count} times</button>;
 }
-
 export default Counter;
+
+
+//return <button onClick={increment}>I have been clicked {count} times</button>;
